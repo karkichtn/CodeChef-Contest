8a673a10d0a227130cb129477533836dd2494cca
KarkiChetan <karkichtn@gmail.com>
2026-06-11 14:58:11 +0530

    Create Q3.cpp

---
+++ June-10-2026/Q3.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+    cin >> t;
+
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        
+
+        vector<long long> sum(n, 0);
+        int card = k;
+
+        
+        bool forward = true;
+
+        while (card > 0) {
+
+            if (forward) {
+
+                for (int i = 0; i < n && card > 0; i++) {
+                    sum[i] += card;
+                    card--;
+                }
+
+            } else {
+
+                for (int i = n - 1; i >= 0 && card > 0; i--) {
+                    sum[i] += card;
+                    card--;
+                }
+
+            }
+
+            forward = !forward;
+        }
+
+        long long ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            ans = max(ans, sum[i]);
+        }
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
